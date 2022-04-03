e15ffd1a9fe3132f25bdfd895dade8d68cfab9dd
Cathy wei <cathywei@CathydeMacBook-Pro.local>
2022-04-02 18:34:44 -0700

    layout, text, image, and anim

--- app/src/main/java/com/example/mobile_final/MainActivity.java
+++ app/src/main/java/com/example/mobile_final/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.mobile_final;
 
+import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView imageView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);        setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.text);
+
+        imageView.setAnimation(topAnim);
+        textView.setAnimation(bottomAnim);
     }
 }
