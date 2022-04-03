12f8c96c0a7731567ce83a9ee1291d110bac0464
Cathy wei <cathywei@CathydeMacBook-Pro.local>
2022-04-02 18:40:59 -0700

    first part done

--- app/src/main/java/com/example/mobile_final/MainActivity.java
+++ app/src/main/java/com/example/mobile_final/MainActivity.java
@@ -4,7 +4,9 @@ import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,6 +14,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH = 3300;
 
     Animation topAnim, bottomAnim;
     ImageView imageView;
@@ -30,5 +34,14 @@ public class MainActivity extends AppCompatActivity {
 
         imageView.setAnimation(topAnim);
         textView.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH);
     }
 }
