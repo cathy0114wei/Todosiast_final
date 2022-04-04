0aabff6b96cc21c8b6416993d42fa34f3e0214d1
joycooperzzz <yan.yan1@northeastern.edu>
2022-04-03 18:31:12 -0700

    yy day 1

--- app/src/main/java/com/example/mobile_final/LoginActivity.java
+++ app/src/main/java/com/example/mobile_final/LoginActivity.java
@@ -1,15 +1,24 @@
 package com.example.mobile_final;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,6 +26,10 @@ public class LoginActivity extends AppCompatActivity {
     private EditText loginEmail, loginPwd;
     private Button loginBtn;
     private TextView loginQn;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +39,9 @@ public class LoginActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.loginToolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Login");
+
+        mAuth = FirebaseAuth.getInstance();
+        loader = new ProgressDialog(this);
 
         loginEmail = findViewById(R.id.loginEmail);
         loginPwd = findViewById(R.id.loginPassword);
@@ -37,6 +53,42 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
                 startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPwd.getText().toString().trim();
+                if (TextUtils.isEmpty(email)){
+                    loginEmail.setError("Email is required!");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    loginPwd.setError("Password required!");
+                    return;
+                } else {
+                    loader.setMessage("Login in progress");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                                loader.dismiss();
+                            } else {
+                                String error = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Login falied" + error, Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+                        }
+                    });
+                }
+
             }
         });
     }
--- app/src/main/java/com/example/mobile_final/RegistrationActivity.java
+++ app/src/main/java/com/example/mobile_final/RegistrationActivity.java
@@ -1,15 +1,27 @@
 package com.example.mobile_final;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException;
 
 public class RegistrationActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -17,6 +29,9 @@ public class RegistrationActivity extends AppCompatActivity {
     private EditText RegEmail, RegPwd;
     private Button RegBtn;
     private TextView RegnQn;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +43,9 @@ public class RegistrationActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Registration");
 
+        mAuth = FirebaseAuth.getInstance();
+        loader = new ProgressDialog(this);
+
         RegEmail = findViewById(R.id.RegistrationEmail);
         RegPwd = findViewById(R.id.RegistrationPassword);
         RegBtn = findViewById(R.id.RegistrationButton);
@@ -38,6 +56,43 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+        RegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = RegEmail.getText().toString().trim();
+                String password = RegPwd.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    RegEmail.setError("Email is required!");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    RegPwd.setError("Password required!");
+                    return;
+                } else {
+                    loader.setMessage("Registration in progress");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Intent intent = new Intent(RegistrationActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+                                loader.dismiss();
+                            } else {
+                                String error = task.getException().toString();
+                                Toast.makeText(RegistrationActivity.this, "Registration failed" + error, Toast.LENGTH_SHORT).show();
+                                loader.dismiss();
+                            }
+
+                        }
+                    });
+                }
+
             }
         });
     }
