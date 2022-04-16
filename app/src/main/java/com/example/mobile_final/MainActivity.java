d267d2be21703b507cd905e3b65f8f552ddba7f4
siyuh3 <meikoshans@gmail.com>
2022-04-16 12:38:08 -0700

    delete the unnecessary import & reformat the code

--- app/src/main/java/com/example/mobile_final/MainActivity.java
+++ app/src/main/java/com/example/mobile_final/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.mobile_final;
 
-import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -24,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
 
         topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
         bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
