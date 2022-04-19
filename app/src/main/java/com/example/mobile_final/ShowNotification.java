cbeed372d1d60b65e06ce916723b16385cbfa4c3
siyuh3 <meikoshans@gmail.com>
2022-04-18 22:28:50 -0700

    update notification activity

--- app/src/main/java/com/example/mobile_final/ShowNotification.java
+++ app/src/main/java/com/example/mobile_final/ShowNotification.java
@@ -7,5 +7,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
 public class ShowNotification {
+    private final static String TAG = "ShowNotification";
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//
+//        Intent mainIntent = new Intent(this, MainActivity.class);
+//
+//        NotificationManager notificationManager
+//                = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+//
+
+//    }
 }
