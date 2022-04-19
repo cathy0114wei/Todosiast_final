9132a5fda4c1db2c09f1f4bb351806a77a7ef886
Cathy wei <cathywei@CathydeMacBook-Pro.local>
2022-04-19 16:47:53 -0700

    Send Email finally Finishgit add .

--- app/src/main/java/com/example/mobile_final/SendEmail.java
+++ app/src/main/java/com/example/mobile_final/SendEmail.java
@@ -1,8 +1,10 @@
 package com.example.mobile_final;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.wifi.ScanResult;
 import android.os.Bundle;
 //import android.se.omapi.Session;
 import android.os.Message;
@@ -12,14 +14,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class SendEmail extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
 
-//    private String realPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,59 +43,72 @@ public class SendEmail extends AppCompatActivity {
         EditText sendTo = (EditText) findViewById(R.id.emailAddress);
         EditText psw = (EditText) findViewById(R.id.emailPassword);
 
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        String onlineUserID = mUser.getUid();
+
         Intent intent = getIntent();
         final String username = intent.getStringExtra("username");
-//        final String password = intent.getStringExtra("password");
-
-//        realPassword = psw.getText().toString();
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Toast.makeText(getApplicationContext(), psw.getText().toString(), Toast.LENGTH_LONG).show();
-//                Toast.makeText(getApplicationContext(), sendTo.getText().toString(), Toast.LENGTH_LONG).show();
-//                Toast.makeText(getApplicationContext(), username, Toast.LENGTH_LONG).show();
+                ArrayList<String> message = new ArrayList<>();
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference("tasks").child(onlineUserID);
+                reference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                            Model model = dataSnapshot.getValue(Model.class);
+                            String txt = model.getDate() +" " + model.getTask() + " " +
+                                    model.getDescription() + " " + (model.getFinished()? "Finished" : "Not Finished Yet");
+                            System.out.println(txt);
+                            message.add(txt);
+                        }
+                        Properties props = new Properties();
+                        props.put("mail.smtp.auth", "true");
+                        props.put("mail.smtp.starttls.enable", "true");
+                        props.put("mail.smtp.host", "smtp.gmail.com");
+                        props.put("mail.smtp.port", "587");
+                        Session session = Session.getInstance(props,
+                                new javax.mail.Authenticator(){
+                                    @Override
+                                    protected PasswordAuthentication getPasswordAuthentication(){
+                                        Toast.makeText(getApplicationContext(), "Email Send Successfully", Toast.LENGTH_LONG).show();
+                                        return new PasswordAuthentication(username, psw.getText().toString());
+                                    }
+                                });
+                        try{
+                            MimeMessage message1 = new MimeMessage(session);
+                            message1.setFrom(new InternetAddress(username));
+                            message1.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(sendTo.getText().toString().trim()));
+                            message1.setSubject("Hi, I would like to share my tasks to you!");
+                            String sendVersion = helperFormat(message);
+                            message1.setText(sendVersion);
+                            Transport.send(message1);
+                        } catch (MessagingException e){
+                            throw new RuntimeException(e);
+                        }
+                    }
 
-                //Toast.makeText(SendEmail.this, username, Toast.LENGTH_SHORT).show();
-                String message = "hello I'm Cathy";
-                Properties props = new Properties();
-                props.put("mail.smtp.auth", "true");
-                props.put("mail.smtp.starttls.enable", "true");
-                props.put("mail.smtp.host", "smtp.gmail.com");
-                props.put("mail.smtp.port", "587");
-//                Toast.makeText(getApplicationContext(), "*************************************************************", Toast.LENGTH_LONG).show();
-                Session session = Session.getInstance(props,
-                        new javax.mail.Authenticator(){
-                            @Override
-                            protected PasswordAuthentication getPasswordAuthentication(){
-                                Toast.makeText(getApplicationContext(), "Email Send Successufully!:)", Toast.LENGTH_LONG).show();
-                                return new PasswordAuthentication(username, psw.getText().toString());
-                            }
-                        });
-                try{
-                    MimeMessage message1 = new MimeMessage(session);
-                    message1.setFrom(new InternetAddress(username));
-                    message1.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(sendTo.getText().toString().trim()));
-                    message1.setSubject("Sending Email without opening Gmail Apps");
-                    message1.setText(message);
-                    Transport.send(message1);
-                } catch (MessagingException e){
-                    throw new RuntimeException(e);
-                }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
             }
         });
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
     }
+    public String helperFormat(ArrayList<String> list){
+        String res = "";
+        for(String str : list){
+            res += str + "\n";
+        }
+        return res;
+    }
 }
-
-
-
-
-
-
-
-
-
 
 
 
