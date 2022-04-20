2b5559c77c0f8945cd9365efc334c661d487116d
siyuh3 <meikoshans@gmail.com>
2022-04-19 21:58:05 -0700

    deleted unnecessary import

--- app/src/main/java/com/example/mobile_final/SendEmail.java
+++ app/src/main/java/com/example/mobile_final/SendEmail.java
@@ -20,12 +20,10 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
